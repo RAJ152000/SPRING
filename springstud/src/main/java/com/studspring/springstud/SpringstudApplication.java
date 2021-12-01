97f2ab7ae93f86b65d9b7fcc47946bbb209531ea
RAJ152000 <rajkumart@scrunchworks.com>
2021-12-01 13:08:59 +0530

    CREATE NEW PROJECT - ONETOONE TASK

---
+++ springstud/src/main/java/com/studspring/springstud/SpringstudApplication.java
@@ -0,0 +1,15 @@
+package com.studspring.springstud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstudApplication.class, args);
+		System.out.println("SUCCESS...");
+		
+	}
+
+}
---
+++ springstud/src/test/java/com/studspring/springstud/SpringstudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studspring.springstud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
