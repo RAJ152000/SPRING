71459885695763a27ad15d7566824247f20447ab
RAJ152000 <rajkumart@scrunchworks.com>
2021-12-08 12:24:11 +0530

    create new project - mapping task

---
+++ usermap/src/main/java/com/mapuser/usermap/UsermapApplication.java
@@ -0,0 +1,13 @@
+package com.mapuser.usermap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermapApplication.class, args);
+	}
+
+}
---
+++ usermap/src/test/java/com/mapuser/usermap/UsermapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapuser.usermap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
